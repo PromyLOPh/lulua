f3a7b6b04b4f7f830168ccbff5cc91b8fe92cfbc
Lars-Dominik Braun <lars@6xq.net>
2020-03-07 09:11:18 +0100

    layouts: Add OSX’ Arabic layout
    Looks pretty similar to ASMO663.

--- gen.sh
+++ gen.sh
@@ -2,7 +2,7 @@
 # Generate build.ninja that builds the docs/stats/…
 
 corpusdir=corpus
-layouts="ar-lulua ar-asmo663 ar-linux ar-malas ar-phonetic ar-osman ar-khorshid"
+layouts="ar-lulua ar-asmo663 ar-linux ar-malas ar-phonetic ar-osman ar-khorshid ar-osx"
 layoutsXmodmap="ar-lulua"
 corpora="`ls ${corpusdir}`"
 
